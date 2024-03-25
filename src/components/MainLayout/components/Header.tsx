43feaae13034252760c695c42c0aa120aad19a04
Elena Donina <Elena_Donina@epam.com>
2024-03-25 09:54:50 +0100

    Task-2

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            My Store!
+            Patterns Store
           </Link>
         </Typography>
 
